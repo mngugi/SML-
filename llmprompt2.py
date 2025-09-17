0fd946b81744397c7cdbbdc57e06a57a2bc25a9e
mngugi <mngugi7@hotmail.com>
2025-09-17 18:04:37 +0300

    llmResponse.py

--- llmprompt2.py
+++ llmprompt2.py
@@ -1,8 +1,6 @@
 from langchain_openai import ChatOpenAI
 
-
-
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
-response = llm.invoke("What is the capital of Kenya?")
+response = llm.invoke(" The song what a beautiful name it is by hillsong")
 print(response.content) 
