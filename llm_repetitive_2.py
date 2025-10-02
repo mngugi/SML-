0cb68fb2eb0daf3b2d3908a5afe23983a8d4c0ca
mngugi <mngugi7@hotmail.com>
2025-10-02 15:49:28 +0300

    Create llm_repetitive_2.py

---
+++ llm_repetitive_2.py
@@ -0,0 +1,11 @@
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+to_do_tasks= ['clean the house', 'wash the car', 'buy groceries', 'do the laundry', 'mow the lawn']
+
+for task in to_do_tasks:
+    print(f'the task is to {task}')
+    response = llm.invoke(f'the task is to {task}')
+    print(response.content)
+# %%    
