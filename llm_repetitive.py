786d9b0dc26bff236b49f32a9efa9f5d624b9175
mngugi <mngugi7@hotmail.com>
2025-10-02 14:05:59 +0300

    Create llm_repetitive.py

---
+++ llm_repetitive.py
@@ -0,0 +1,11 @@
+from langchain_openai import ChatOpenAI
+
+name = "Andreas"
+
+prompt = f"""write a poem for {name} birthday, inspired by the name Andreas """
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+response = llm.invoke(prompt)
+print(response.content)
+
+# %%    
