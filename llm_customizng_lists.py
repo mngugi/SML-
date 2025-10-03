9b54729efcec025d1d6bb1390a948f17759d76fb
mngugi <mngugi7@hotmail.com>
2025-10-03 17:56:58 +0300

    Update llm_customizng_lists.py

--- llm_customizng_lists.py
+++ llm_customizng_lists.py
@@ -3,3 +3,17 @@ from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
 
+food_preferences = {
+    "spicy": ["spicy", "hot", "sauce", "chili", "pepper"],
+    "sweet": ["sweet", "sugar", "honey", "jam", "syrup"],
+    "salty": ["salty", "salt", "sodium", "soup", "broth"],
+    "savory": ["savory", "umami", "broth", "soup", "sauce"],
+    "sour": ["sour", "acid", "vinegar", "lemon", "lime"],
+    "bitter": ["bitter", "piquant", "tangy", "astringent", "umami"],
+    "savory": ["savory", "umami", "broth", "soup", "sauce"],
+    "sour": ["sour", "acid", "vinegar", "lemon", "lime"],
+    "bitter": ["bitter", "piquant", "tangy", "astringent", "umami"],
+}
+print(food_preferences)
+print("--------------------------------")
+print(food_preferences.keys())
